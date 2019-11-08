60a7844cc4fb2a0f45aacfda82ccfe7e7c84a746
JeromeLiee <jeromeliee@foxmail.com>
2019-11-08 11:33:11 +0800

    add leetcode 104 problem solution

--- src/_24_tree/TreeTest.java
+++ src/_24_tree/TreeTest.java
@@ -36,6 +36,8 @@ public class TreeTest {
         System.out.println(treeTest.inOrderTraversal(root));
         treeTest.insert(root, 15);
         System.out.println(treeTest.inOrderTraversal(root));
+
+        System.out.println(treeTest.maxDepth(root));
     }
 
     /**
@@ -106,12 +108,31 @@ public class TreeTest {
      * 二叉树的最大深度
      * <p>
      * LeetCode 104
+     * <p>
+     * 给定一个二叉树，找出其最大深度。
+     * <p>
+     * 二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+     * <p>
+     * 说明: 叶子节点是指没有子节点的节点。
+     * <p>
+     * 示例：
+     * 给定二叉树 [3,9,20,null,null,15,7]，
+     * <p>
+     * 3
+     * / \
+     * 9  20
+     * /  \
+     * 15   7
+     * 返回它的最大深度 3 。
      *
      * @param root
      * @return
      */
     public int maxDepth(TreeNode root) {
-        return 0;
+        if (root == null) return 0;
+        if (root.left == null) return maxDepth(root.right) + 1;
+        if (root.right == null) return maxDepth(root.left) + 1;
+        return Math.max(maxDepth(root.right) + 1, maxDepth(root.left) + 1);
     }
 
     /**
